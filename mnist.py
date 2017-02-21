Fixed matrix alignment issues in backprop.

mnist.py
@@ -21,27 +21,38 @@ class mnist(data):
 		images = array("B", data_file.read())
 		labels = array("B", label_file.read())
 
-		for i in range(size):
-			y = np.zeros((10, 1))
-			y[labels[i]] = 1
-
-			x = np.matrix(images[i * rows * columns:
-										(i + 1) * rows * columns]).transpose()
-
-			data_set.append((x, y))
-
-		return data_set
+		return size, images, labels, rows * columns
 
 	@classmethod
 	def load (cls):
 		""" load MNIST training and test data sets """
-		training_set = cls.load_set("train-labels.idx1-ubyte",
+		training_set = []
+		test_set = []
+
+		size, images, labels, pixels = cls.load_set("train-labels.idx1-ubyte",
 													"train-images.idx3-ubyte")
-		test_set = cls.load_set("t10k-labels.idx1-ubyte",
-													"t10k-images.idx3-ubyte")
+		for i in range(size):
+			y = np.zeros((10, 1))
+			y[labels[i]] = 1
+			x = np.matrix(images[i * pixels: (i + 1) * pixels]).transpose()
+			training_set.append((x, y))
+
+		size, images, labels, pixels = cls.load_set("train-labels.idx1-ubyte",
+													"train-images.idx3-ubyte")
+		for i in range(size):
+			x = np.matrix(images[i * pixels: (i + 1) * pixels]).transpose()
+			test_set.append((x, labels[i]))
+
 		return training_set, test_set
 
 	# Needs to be completed
 	@classmethod
 	def validate (cls, test_set, outputs):
-		return
+		num_valid = 0
+
+		for i in range(len(test_set)):
+			x, y = test_set[i]
+			if y == np.argmax(outputs[i]):
+				num_valid = num_valid + 1
+
+		return num_valid / len(test_set)

network.py
@@ -3,21 +3,23 @@ import numpy as np
 class network:
 	""" Neural network class """
 
-	def __init__ (self, network_layers):
+	def __init__ (self, input_size, output_size, hidden_layers):
 		"""
 		initialize network bias and weight matricies
 		with sizes dependent on data set and hidden layer sizes
 		"""
-		self.layers = network_layers
-		self.biases = [np.random.rand(x, 1) for x in network_layers[1:]]
+		self.layers = [input_size]
+		self.layers.extend(hidden_layers)
+		self.layers.append(output_size)
+		self.biases = [np.random.rand(x, 1) for x in self.layers[1:]]
 		self.weights = [np.random.rand(x, y) for x, y in 
-								zip(network_layers[1:], network_layers[0:-1])]
+								zip(self.layers[1:], self.layers[0:-1])]
 
-	def sigmoid (z):
+	def sigmoid (self, z):
 		return 1 / (1 + np.exp(-z))
 
-	def sigmoid_prime (z):
-		return np.multiply(sigmoid(z), (1 - sigmoid(z)))
+	def sigmoid_prime (self, z):
+		return np.multiply(self.sigmoid(z), (1 - self.sigmoid(z)))
 
 	def export(self):
 		""" returns network variables """
@@ -28,7 +30,7 @@ class network:
 		activation = x
 		for i in range(len(self.layers) - 1):
 			z = np.dot(self.weights[i], activation)+ self.biases[i]
-			activation = `sigmoid(z)
+			activation = self.sigmoid(z)
 		return activation
 
 	def backprop(self, x, y):
@@ -43,34 +45,34 @@ class network:
 		for i in range(len(self.layers) - 1):
 			z = np.dot(self.weights[i], a_values[i]) + self.biases[i]
 			z_values.append(z)
-			a_values.append(sigmoid(z))
+			a_values.append(self.sigmoid(z))
 
 		""" error calculation """
-		err = np.multiply(y - a_values[-1], sigmoid_prime(z_values[-1]))
+		err = np.multiply(y - a_values[-1], self.sigmoid_prime(z_values[-1]))
 		delta_biases[-1] = err
 		delta_weights[-1] = np.dot(a_values[-2], err.transpose())
 
 		""" back pass with error calculations """
 		for i in range(len(z_values) - 1):
-			err = np.multiply(np.dot(self.weights[-i], err), \
-					sigmoid_prime(z_values[-i-2]))
+			err = np.multiply(np.dot(self.weights[-i-1].transpose(), err), \
+					self.sigmoid_prime(z_values[-i-2]))
 
 			delta_biases[-i-2] = err
 			delta_weights[-i-2] = np.dot(a_values[-i-3], err.transpose())
 
 		return delta_biases, delta_weights
 
-	def train(self, training_data, target_ouput, learning_rate):
+	def train(self, training_data, learning_rate):
 		""" cycle through training data then update network """
 		d_w_total = [np.zeros(x.shape) for x in self.weights]
 		d_b_total = [np.zeros(x.shape) for x in self.biases]
 
-		for x, y in zip(training_data, target_output):
-			d_w, d_b = backprop(x, y)
-			d_w_total = [old + new for old, new in zip(d_w_total, d_w)]
+		for x, y in training_data:
+			d_b, d_w = self.backprop(x, y)
+			d_w_total = [old + new.transpose() for old, new in zip(d_w_total, d_w)]
 			d_b_total = [old + new for old, new in zip(d_b_total, d_b)]
 
-		self.weights = [old - learning_rate/len(training_data) * total \
-							for old, total in zip(self.weight, d_w_total)]
-		self.biases = [old - learning_rate/len(training_data) * total \
+		self.weights = [old - learning_rate/len(training_data) * total
+							for old, total in zip(self.weights, d_w_total)]
+		self.biases = [old - learning_rate/len(training_data) * total
 							for old, total in zip(self.biases, d_b_total)]